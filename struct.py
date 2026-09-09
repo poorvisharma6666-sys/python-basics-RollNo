e0c3ffef6660d407a9a56a42d38df5a1ede9906a
poorvisharma6666-sys <poorvisharma.6666@gmail.com>
2026-09-09 09:16:22 +0530

    Add fibonacci and struct programs

---
+++ struct.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Poorvi",
+    "roll number": "2365",
+    "course": "BTech CSE AI/ML"
+}
+
+print("Student Details")
+print("Name:", student["name"])
+print("Roll number:", student["roll number"])
+print("Course:", student["course"])
