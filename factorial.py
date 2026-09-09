c9831b917d58b867d81a3804759db07722e2241f
poorvisharma6666-sys <poorvisharma.6666@gmail.com>
2026-09-09 09:20:46 +0530

    Add comments to factorial program

--- factorial.py
+++ factorial.py
@@ -1,5 +1,12 @@
+#  Take a number as input from the user
 n = int(input("Enter a number:"))
+
+# Start factorial with 1
 factorial = 1
+
+# Multiply numbers from 1 to n
 for i in range(1, n+1):
   factorial *=i
+
+  # Display the final factorial
   print("Fcatorial =", factorial)
